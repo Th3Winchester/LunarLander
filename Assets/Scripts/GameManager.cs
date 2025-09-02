685365544cdabfa49eb244c99a59490e2c402dff
Th3Winchester <kacperpajor2003@gmail.com>
2025-09-02 12:20:19 +0200

    add GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Lander lander;
+
+    private int score;
+
+    private void Start()
+    {
+        lander.OnCoinPickup += Lander_OnCoinPickup;
+    }
+
+    private void Lander_OnCoinPickup(object sender, System.EventArgs e)
+    {
+        AddScore(500);
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        Debug.Log("Score: " + score);
+    }
+}
