23037216dac89ace2d88652898f3aac9dce81f1d
Th3Winchester <kacperpajor2003@gmail.com>
2025-09-02 14:54:37 +0200

    update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
     private int score;
+    private float time;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         Lander.Instance.OnCoinPickup += Lander_OnCoinPickup;
         Lander.Instance.OnLanded += Lander_OnLanded;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
     }
 
     private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e)
@@ -25,5 +38,15 @@ public class GameManager : MonoBehaviour
     {
         score += amount;
         Debug.Log("Score: " + score);
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public float GetTime()
+    {
+        return time;
     }
 }
